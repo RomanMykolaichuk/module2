88295b685e2b538040f172c31ddc0100e919459a
38068 <38068@DESKTOP-SRPFKQ2>
2020-10-09 16:15:57 +0300

    5 module solution added preparing for classes 05.10

---
+++ ngJS/module1_solution/js/app.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('M1app', [])
+            .controller('M1Controller', M1Controller);
+
+    M1Controller.$inject = ['$scope', '$filter'];
+    function M1Controller($scope) {
+        $scope.dishes = "";
+        $scope.message = "message";
+
+        $scope.m1Click = function () {
+            if ($scope.dishes.length === 0) {
+                $scope.message = "Please enter data first!";
+                
+            } else {
+                
+            }
+            ;
+
+
+
+            $scope.message = 'clicked!';
+
+        };
+
+
+    }
+
+})();
+
