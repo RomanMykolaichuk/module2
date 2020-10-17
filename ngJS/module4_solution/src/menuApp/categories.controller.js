2e6a0023be9e6e70ff43b7a620408df11d93751b
38068 <38068@DESKTOP-SRPFKQ2>
2020-10-17 21:47:03 +0300

    first commit

---
+++ ngJS/module4_solution/src/menuApp/categories.controller.js
@@ -0,0 +1,16 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('CategoriesController', CategoriesController);
+
+
+
+
+CategoriesController.$inject = ['categories'];
+function CategoriesController(categories) {
+  var categoriesList = this;
+  categoriesList.categories = categories;
+}
+
+})();
